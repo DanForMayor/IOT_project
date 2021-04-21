6365f36a2f355afcc82c0c60f07a5554d5140ea6
DanForMayor <danmccann0@gmail.com>
2021-04-20 21:39:06 -0600

    Added an API for the device to send data to, which then emits it to all the clients

--- index.js
+++ index.js
@@ -4,19 +4,42 @@ const express = require('express');
 const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
 
 // The page is going to be in EJS
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded());
 
 // Set the / path to be the index.ejs file in the views folder
 app.get('/',(req, res) =>{
   res.render('index.ejs')
 });
 
+// This sets an API endpoint for the device
+app.post('/sendDeviceData',(req, res) =>{
+  console.log(req.body)
+
+  // Use a secret API token to prevent others from using this API
+  if(req.body.apiKey == "_xY|Ytr#T!*MOj>ladOc"){
+
+    // Send the data to the clients
+    io.sockets.emit('message', {"tmp":req.body.tmp, "x":req.body.x, "y":req.body.y, "z":req.body.z})
+
+    // Just return the success if everything worked
+    res.sendStatus(200);
+  }else{
+    // Send back a forbidden if they entered the wrong API key
+    res.sendStatus(403);
+  }
+  
+});
+
 // Set the port for the server to listen on
 server.listen(3000, ()=>{
   console.log("server running on port 3000...")
 })
+
+
 
 // Set the 'connection' socket for detecting when a client has connected
 io.on('connection', (socket) => {
@@ -25,22 +48,12 @@ io.on('connection', (socket) => {
   // Show the client ID in console
   console.log("User connected: " + socket.id);
 
-  // Start sending the client the temperature data, every second
-  setInterval(() => {
-
-    child_process.exec('python3 /home/pi/gyro_temp.py', (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        return;
-      }
-      socket.emit('message',JSON.parse(stdout));
-    });
-  }, 1000);
   
   // Check for client disconnection
   socket.on("disconnect", (reason) => {
     //TODO: I eventually want this to decriment a client count that will print on an LCD display
     console.log(reason)
+    // clearInterval(intervalId)
   });
 
 });
